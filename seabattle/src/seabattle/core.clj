b9cb065d281d71a1d3eb471e89f8b1cce600c08f
Pavel Osipov <warlock88@yandex.ru>
2017-03-24 20:58:23 +0300

    Add function make-board-text

--- seabattle/src/seabattle/core.clj
+++ seabattle/src/seabattle/core.clj
@@ -1,24 +1,9 @@
-;;  OK, let the board be a hashmap with vector keys and following values:
-;;       :empty -- cell empty
-;;       :ship-alive -- here is alive part of a ship
-;;       :ship-dead -- here is dead part of ship
-;;       :busy -- cell is near a ship, so another ship can't be placed here
-;;
-;;  Also, let the board be actually 12x12 with :busy values on the borders,
-;;  just to save program's logic from decisions abut existence of the cells
-;;
-;;
-;;  Also, let's keep a value of ships parts already hit, if this value reaches 4 + 3 + 3 + 2 + 2 + 2 + 1 + 1 + 1 + 1 = 20
-;;  game is over
-;;
-;;
-
 (ns seabattle.core
   (:gen-class))
 
 (defn make-empty-board
   "Makes empty board as hashmap, returns board"
-  [ ]
+  []
   (loop [n 11
              m 11
              board {}]
@@ -29,14 +14,17 @@
             (recur n (dec m)  (assoc board [n m] :empty))
             (recur (dec n) 11 (assoc board [n m] :empty))))))
 
-(defn make-border-for-board
+(defn make-border
   "Makes :busy border for the board. Returns new board"
   [board]
   (loop [x 0
-              new-board board]
+         new-board board]
     (if (> x 11)
       new-board
-      (recur (inc x) (assoc new-board (vector x 0 ) :busy (vector 0 x ) :busy  (vector x  11) :busy  (vector  11 x)  :busy)))))
+      (recur (inc x) (assoc new-board (vector x 0 ) :busy
+                                      (vector 0 x ) :busy
+                                      (vector x  11) :busy
+                                      (vector  11 x)  :busy)))))
 
 (defn is-cell-empty?
   "Tests if given cell on the board is empty"
@@ -53,7 +41,7 @@
   [board n m]
   (= :ship-alive (get  board (vector n m ))))
 
-(defn is-cell-alive?
+(defn is-cell-dead?
   "Tests if given cell on the board is dead part of the ship"
   [board n m]
   (= :ship-dead (get  board (vector n m ))))
@@ -71,12 +59,13 @@
 (defn can-place-ship-horizontal
   "Tests if ship with given size can be placed horizontally with given left point"
   [board row leftcol size]
-   (loop [c leftcol
-                d 1
-               ans true]
-      (if (> d size)
-          ans
-          (recur (inc r) (inc d) (and ans (or (is-cell-empty? board r col ) (is-cell-busy? board r col)))))))
+;;   (loop [c leftcol
+;;                d 1
+;;               ans true]
+;;      (if (> d size)
+;;         ans
+;;          (recur (inc r) (inc d) (and ans (or (is-cell-empty? board r col ) (is-cell-busy? board r col))))))
+  )
 
 (defn place-4-ship-vertical
   "Places 4-cell ship vertically, it's assumed this ship will be first, so function is to be called on empty board. Returs new board."
@@ -123,13 +112,30 @@
    "Places 3-cell ship on  the board, and returns new board"
   [board]
 
+
   )
+
+
+(defn make-board-text
+  "Prepares the board as single strings and return that string"
+  [board]
+  (loop [n 0
+         m 0
+         board-s ""]
+    (if (> n 11)
+      board-s
+        (if (<= m 11)
+          (recur n (inc m) (if (is-cell-busy? board n m)
+                             (str board-s "#")
+                             (str board-s ".")))
+          (recur (inc n) 0 (str board-s "\n"))))))
+
 
 (defn show-board
   "Shows the board state. Used for debug purposes only"
   [board]
   (loop [n 0
-              m 0]
+         m 0]
     (if (> n 11)
       true
       (do
