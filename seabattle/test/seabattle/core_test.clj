ac6f512319eaa260932f6d85fa50bffe360ce309
Pavel Osipov <warlock88@yandex.ru>
2017-03-24 20:57:45 +0300

    Add tests for empty board

--- seabattle/test/seabattle/core_test.clj
+++ seabattle/test/seabattle/core_test.clj
@@ -2,6 +2,47 @@
   (:require [clojure.test :refer :all]
             [seabattle.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def empty-board
+"............
+............
+............
+............
+............
+............
+............
+............
+............
+............
+............
+............
+")
+
+
+(def board-with-border
+"############
+#..........#
+#..........#
+#..........#
+#..........#
+#..........#
+#..........#
+#..........#
+#..........#
+#..........#
+#..........#
+############
+")
+
+
+
+(deftest is-board-good
+  (testing "Board is made correct"
+    (let [board (make-empty-board)]
+    (is (= (count (make-board-text board))  (count empty-board)) "Wrong board text count")
+    (is (= (make-board-text board) empty-board) "Wrong board text by itself")))
+
+  (testing "Board has good border"
+    (let [board (make-border (make-empty-board))]
+    (is (= (count (make-board-text board))  (count board-with-border)) "Wrong board text count")
+    (is (= (make-board-text board) board-with-border) "Wrong board text by itself"))))
