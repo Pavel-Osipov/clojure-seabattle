6d4c95df2ca6d149525dc08274123634f3b220e3
Pavel Osipov <warlock88@yandex.ru>
2017-03-26 08:59:38 +0300

    Redesing setup funcs

--- seabattle/src/seabattle/setup.clj
+++ seabattle/src/seabattle/setup.clj
@@ -5,14 +5,13 @@
   "Makes empty board as hashmap, returns board"
   []
   (loop [n 11
-             m 11
-             board {}]
-
-        (if ( < n 0)
-          board
-          (if (> m 0)
-            (recur n (dec m)  (assoc board [n m] :empty))
-            (recur (dec n) 11 (assoc board [n m] :empty))))))
+         m 11
+        board {}]
+    (if ( < n 0)
+      board
+      (if (> m 0)
+          (recur n (dec m)  (assoc board [n m] :empty))
+          (recur (dec n) 11 (assoc board [n m] :empty))))))
 
 (defn make-border
   "Makes :busy border for the board. Returns new board"
@@ -23,81 +22,79 @@
       new-board
       (recur (inc x) (assoc new-board (vector x 0 )  :busy
                                       (vector 0 x )  :busy
-                                      (vector x  11) :busy
-                                      (vector  11 x) :busy)))))
+                                      (vector x 11)  :busy
+                                      (vector 11 x)  :busy)))))
 
 (defn surround-ship
   "Surround the given ship with :busy cells.
   Returns new board."
   [board ship-type ship-size start other]
   (loop [new-board board
-         d 1
-         n (dec start)
-         m other]
-    (if (> d (+ ship-size 2))
+         cellnum 1
+         n (dec start)]
+    (if (> cellnum (+ ship-size 2))
       (if (= ship-type :hor)
-          (assoc new-board (vector m (dec start))         :busy
-                           (vector m (+ start ship-size)) :busy)
-          (assoc new-board (vector (dec start) m)         :busy
-                           (vector (+ start ship-size) m) :busy))
+          (assoc new-board (vector other (dec start))         :busy
+                           (vector other (+ start ship-size)) :busy)
+          (assoc new-board (vector (dec start) other)         :busy
+                           (vector (+ start ship-size) other) :busy))
       (recur (if (= ship-type :hor)
-                 (assoc new-board (vector (dec m) n) :busy
-                                  (vector (inc m) n) :busy)
-                 (assoc new-board (vector n (dec m)) :busy
-                                  (vector n (inc m)) :busy))
-             (inc d)
-             (inc n)
-             m))))
+                 (assoc new-board (vector (dec other) n) :busy
+                                  (vector (inc other) n) :busy)
+                 (assoc new-board (vector n (dec other)) :busy
+                                  (vector n (inc other)) :busy))
+             (inc cellnum)
+             (inc n)))))
 
 (defn place-ship-vertical
   "Places ship vertically, assumed that it's possible
   Returs new board."
   [board ship-size start-row ncol]
   (loop [new-board board
-         d 1
-         r start-row
-         c ncol]
-    (if (> d ship-size)
+         cellnum 1
+         r start-row]
+    (if (> cellnum ship-size)
       (surround-ship new-board :vert ship-size start-row ncol)
-      (recur (assoc new-board (vector r c) :ship-alive)
-             (inc d)
-             (inc r)
-             c))))
+      (recur (assoc new-board (vector r ncol) :ship-alive)
+             (inc cellnum)
+             (inc r)))))
 
 (defn place-ship-horizontal
   "Places 4-cell ship horizontally, assumed that it's possible
   Returs new board."
   [board ship-size start-col nrow]
   (loop [new-board board
-         d 1
-         r nrow
+         cellnum 1
          c start-col]
-    (if (> d ship-size)
+    (if (> cellnum ship-size)
       (surround-ship new-board :hor ship-size start-col nrow)
-      (recur (assoc new-board (vector r c) :ship-alive)
-             (inc d)
-             r
+      (recur (assoc new-board (vector nrow c) :ship-alive)
+             (inc cellnum)
              (inc c)))))
 
 (defn can-place-ship-vertical?
   "Tests if ship with given size can be placed vertically with given top point"
   [board toprow ncol ship-size]
    (loop [r toprow
-          d 1
+          cellnum 1
           ans true]
-      (if (> d ship-size)
+      (if (> cellnum ship-size)
           ans
-          (recur (inc r) (inc d) (and ans (is-cell-empty? board r ncol))))))
+          (recur (inc r)
+                 (inc cellnum)
+                 (and ans (is-cell-empty? board r ncol))))))
 
 (defn can-place-ship-horizontal?
   "Tests if ship with given size can be placed horizontally with given left point"
   [board nrow leftcol ship-size]
    (loop [c leftcol
-          d 1
+          cellnum 1
           ans true]
-      (if (> d ship-size)
+      (if (> cellnum ship-size)
           ans
-          (recur (inc c) (inc d) (and ans (is-cell-empty? board nrow c))))))
+          (recur (inc c)
+                 (inc cellnum)
+                 (and ans (is-cell-empty? board nrow c))))))
 
 (defn place-ship
    "Places ship on the board, in random place and returns new board"
--- seabattle/test/seabattle/core_test.clj
+++ seabattle/test/seabattle/core_test.clj
@@ -41,10 +41,10 @@
 (deftest is-board-good
   (testing "Board is made correct"
     (let [board (make-empty-board)]
-    (is (= (count (make-board-text board))  (count empty-board)) "Wrong board text count")
-    (is (= (make-board-text board) empty-board) "Wrong board text by itself")))
+    (is (= (count (make-board-debug-text board))  (count empty-board)) "Wrong board text count")
+    (is (= (make-board-debug-text board) empty-board) "Wrong board text by itself")))
 
   (testing "Board has good border"
     (let [board (make-border (make-empty-board))]
-    (is (= (count (make-board-text board))  (count board-with-border)) "Wrong board text count")
-    (is (= (make-board-text board) board-with-border) "Wrong board text by itself"))))
+    (is (= (count (make-board-debug-text board))  (count board-with-border)) "Wrong board text count")
+    (is (= (make-board-debug-text board) board-with-border) "Wrong board text by itself"))))
