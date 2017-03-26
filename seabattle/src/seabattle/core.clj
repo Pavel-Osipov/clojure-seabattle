b529ca0bafc2aa4483e20291bffa5f98c7b71907
Pavel Osipov <warlock88@yandex.ru>
2017-03-26 08:17:31 +0300

    Create logicals namespace

--- seabattle/src/seabattle/core.clj
+++ seabattle/src/seabattle/core.clj
@@ -1,30 +1,7 @@
 (ns seabattle.core
   (:require [seabattle.setup :refer :all])
+  (:require [seabattle.logicals :refer :all])
   (:gen-class))
-
-
-(defn is-cell-empty?
-  "Tests if given cell on the board is empty"
-  [board n m]
-  (= :empty (get  board (vector n m ))))
-
-
-(defn is-cell-busy?
-  "Tests if given cell on the board is busy"
-  [board n m]
-  (= :busy (get  board (vector n m ))))
-
-
-(defn is-cell-alive?
-  "Tests if given cell on the board is alive part of the ship"
-  [board n m]
-  (= :ship-alive (get  board (vector n m ))))
-
-
-(defn is-cell-dead?
-  "Tests if given cell on the board is dead part of the ship"
-  [board n m]
-  (= :ship-dead (get  board (vector n m ))))
 
 
 (defn can-place-ship-vertical?
