497245307fd70a5436ebde1a2f3fbd309c0445f3
Pavel Osipov <warlock88@yandex.ru>
2017-08-13 10:48:58 +0300

    Add test dir to  project.clj

--- seabattle/project.clj
+++ seabattle/project.clj
@@ -1,9 +1,10 @@
 (defproject seabattle "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure learning seabattle project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot seabattle.core
   :target-path "target/%s"
+  :test-paths ["test"]
   :profiles {:uberjar {:aot :all}})
