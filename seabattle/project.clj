9902f24be33a40abf0ec36a7ba598ed5aafebb58
Pavel Osipov <warlock88@yandex.ru>
2017-08-13 11:21:29 +0300

    Add linter and code analizer and refactor

--- seabattle/project.clj
+++ seabattle/project.clj
@@ -3,8 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]]
+
   :main ^:skip-aot seabattle.core
+
   :target-path "target/%s"
   :test-paths ["test"]
-  :profiles {:uberjar {:aot :all}})
+
+  :profiles
+    {:uberjar {:aot :all}
+     :dev
+      [{:plugins [[lein-kibit "0.1.3"]
+                  [jonase/eastwood "0.2.3"]]}]})
--- seabattle/src/seabattle/logicals.clj
+++ seabattle/src/seabattle/logicals.clj
@@ -1,25 +1,28 @@
 (ns seabattle.logicals)
 
+(defn get-cell [board n m]
+  (get board (vector n m)))
+
 (defn is-cell-empty?
   "Tests if given cell on the board is empty"
   [board n m]
-  (= :empty (get  board (vector n m ))))
+  (= :empty (get-cell board n m)))
 
 
 (defn is-cell-busy?
   "Tests if given cell on the board is busy"
   [board n m]
-  (= :busy (get  board (vector n m ))))
+  (= :busy (get-cell board n m)))
 
 
 (defn is-cell-alive?
   "Tests if given cell on the board is alive part of the ship"
   [board n m]
-  (= :ship-alive (get  board (vector n m ))))
+  (= :ship-alive (get-cell board n m)))
 
 
 (defn is-cell-dead?
   "Tests if given cell on the board is dead part of the ship"
   [board n m]
-  (= :ship-dead (get  board (vector n m ))))
+  (= :ship-dead (get-cell board n m)))
 
